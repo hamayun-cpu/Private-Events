b71d33aaf1195a2c31ba07c8b41096ccb51bbb41
hamayun <hamayun.waheed1999@gmail.com>
2021-01-14 22:21:11 +0500

    completed the signin and signup feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,31 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[show]
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
-    @user = User.find(params[:id])
+    if current_user
+      @user = current_user
+    end
   end
 
   private
 
-  def users_params
-    paramas.require(:user).permit(:name)
+  def user_params
+    params.require(:user).permit(:name)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[new create show]
   root 'users#show'
+  get '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
